d005c44deee0acd0417cd1ee265111b0e7289dcd
naylorpmax-joyent <max.naylor@joyent.com>
2025-04-17 13:02:07 -0400

    fixin

--- writer.go
+++ writer.go
@@ -1,6 +1,7 @@
 package iscsi
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log/slog"
@@ -12,6 +13,8 @@ type writer struct {
 	offset    int64
 	blocksize int64
 }
+
+var ErrMaxLBA = errors.New("sorry mate you've gone too far")
 
 func Writer(dev *device) (*writer, error) {
 	c, err := dev.ReadCapacity16()
@@ -36,37 +39,40 @@ func (w *writer) WriteAt(p []byte, off int64) (n int, err error) {
 		return 0, io.EOF
 	}
 	logger().Debug("WriteAt", slog.Int("bytes", len(p)), slog.Int("offset", int(off)))
+
 	// find our starting lba
 	startBlock := off / w.blocksize
 	endOffset := len(p) + int(off)
 	blocks := (endOffset-int(off))/int(w.blocksize) + 1
 	blocks = min(blocks, int(w.lba)-int(startBlock))
 
-	// handle EOF
-	if (endOffset/int(w.blocksize)) > int(w.lba) || blocks == int(w.lba-startBlock) {
-		err = io.EOF
-		logger().Debug("reached EOF", slog.Int("lba", int(w.lba)), slog.Int("endOffset", endOffset))
-		blocks = int(w.lba - startBlock)
-	} else if endOffset%int(w.blocksize) != 0 {
-		// if endoffset is not block aligned then we need to read one more block
-		blocks++
-	}
-
+	var written int
 	blocks = min(blocks, int(w.lba)-int(startBlock))
-	for block := range blocks {
+	for block := 0; block < blocks; block++ {
 		start := startBlock * int64(block)
 		end := min(start+w.blocksize, int64(len(p)))
 
+		// pad data if it's smaller than a block
+		data := p[start:end]
+		if int64(len(p)) < w.blocksize {
+			data = make([]byte, w.blocksize)
+			copy(data, p[start:end])
+
+			end = w.blocksize
+		}
+
 		writeErr := w.dev.Write16(Write16{
 			LBA:       int(start),
-			BlockSize: int(end - start),
-			Data:      p[start:end],
+			BlockSize: int(w.blocksize),
+			Data:      data,
 		})
 		if writeErr != nil {
-			return 0, fmt.Errorf("iscsi device write error: %w", writeErr)
+			return written, fmt.Errorf("iscsi device write error: %w", writeErr)
 		}
+
+		written += len(data)
 	}
 
 	logger().Debug("finished write", slog.Int("length", len(p)))
-	return len(p), err
+	return written, err
 }
